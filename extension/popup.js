e5395284712903bc6b65e5c81436bf9d2a4fe08b
Michael Peng <michael@mcmoo.org>
2022-03-11 20:34:30 -0500

    Add experimental extension code
    Co-authored-by: Alex de la Iglesia <alexander.delaiglesia@gmail.com>

---
+++ extension/popup.js
@@ -0,0 +1,7 @@
+chrome.tabs.create({url: 'controller.html'});
+
+/*
+
+chrome.tabs.create({url: 'https://www.linkedin.com/mynetwork/invite-connect/connections/'}).then(t => {while (tchrome.scripting.executeScript({target: {tabId: t.id}, files: ["collectContacts.js"]}, console.info))
+
+*/
