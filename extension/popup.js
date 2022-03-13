53f0d48138a354571b140bf12b0e3254246e6812
Alexander Victor De La Iglesia <alexander.delaiglesia@gmail.com>
2022-03-13 04:40:23 -0400

    More work on accessing user info, also added a simple extension for sample code

--- extension/popup.js
+++ extension/popup.js
@@ -1,7 +1,47 @@
-chrome.tabs.create({url: 'controller.html'});
 
-/*
+document.querySelector('#goto-linkedin').addEventListener('click', () => {
+   chrome.tabs.create({
+      active: true,
+      url: 'https://www.linkedin.com/mynetwork/invite-connect/connections/'
+   });
+});
 
-chrome.tabs.create({url: 'https://www.linkedin.com/mynetwork/invite-connect/connections/'}).then(t => {while (tchrome.scripting.executeScript({target: {tabId: t.id}, files: ["collectContacts.js"]}, console.info))
+document.querySelector('#load-users').addEventListener('click', async () => {
 
-*/
+   let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+
+   chrome.scripting.executeScript({
+      target: { tabId: tab.id },
+      function: handleLinkedInPage,
+   });
+
+})
+
+const handleEntry = entry => {
+   chrome.tabs.create({
+     active: true,
+     url: entry.link
+  }, tab => {
+     console.log(`Loaded tab for ${entry.name} with index ${tab.id}/${tab.status}`)
+  });
+}
+const handleLinkedInPage = () => {
+   const links = Array.from(document.querySelectorAll('.mn-connection-card__link'));
+   const entries = links.map(link => ({
+       name: link.querySelector('.mn-connection-card__name').innerText,
+       occupation: link.querySelector('.mn-connection-card__occupation').innerText,
+       link: link.href
+   })).slice(0, 3);
+   // entries = entries.slice(0, 3);
+   console.log(entries); // debug
+   
+   // Go to each page
+   entries.forEach((entry, i) => {
+      let windowRef = window.open(entry.link, '_blank');
+      setTimeout(() => {
+         alert(windowRef.document.querySelector('#top-card-text-details-contact-info').textContent)
+      }, 2000)
+      // This page has
+      // https://www.linkedin.com/in/simarpreet-singh-84a599190/overlay/contact-info/
+   });
+}
