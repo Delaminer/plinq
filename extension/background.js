063db1903eaf0316c4310118672ebb34856bb4fb
Michael Peng <michael@mcmoo.org>
2022-04-08 17:39:02 -0400

    Complete LinkedIn import data flow

--- extension/background.js
+++ extension/background.js
@@ -1,3 +1,12 @@
+/*
+  firstName
+  lastName
+  picture
+  email
+  phone
+  linkedIn
+  contactHistory
+*/
 let baseData = {
     contacts: [],
 };
@@ -27,16 +36,52 @@ function setupRequestIntercept() {
 
 chrome.runtime.onMessage.addListener((msg, sender, respond) => {
   console.info('got message', msg);
-  if (msg.module === 'liImport' && msg.topic === 'enabled?') {
-    async function res() {
-      const enabled = (await chrome.storage.sync.get('liImporting')).liImporting;
-      if (enabled) {
-        await chrome.storage.sync.set({liImporting: false});
+  if (msg.module === 'liImport') {
+    if (msg.topic === 'enabled?') {
+      async function res() {
+        const enabled = (await chrome.storage.sync.get('liImporting')).liImporting;
+        if (enabled) {
+          await chrome.storage.sync.set({liImporting: false});
+        }
+        respond({value: enabled});
+        console.info('responded with', {value:enabled});
       }
-      respond({value: enabled});
-      console.info('responded with', {value:enabled});
+      res();
+      return true;
+    } else if (msg.topic === 'existing?') {
+      async function res() {
+        const store = (await chrome.storage.sync.get('plinq')).plinq;
+        const out = [];
+        for (let i = 0; i < msg.query.length; ++i) {
+          if (store.contacts.some(ct => ct.linkedIn === 'https://www.linkedin.com/in/' + encodeURIComponent(msg.query[i]))) {
+            out.push(i);
+          }
+        }
+        respond({existing: out});
+      }
+      res();
+      return true;
+    } else if (msg.topic === 'upsert') {
+      async function res() {
+        const store = (await chrome.storage.sync.get('plinq')).plinq;
+        const updatingLinkedInUrls = new Set();
+        for (const item of msg.items) {
+          if (item.linkedIn) {
+            updatingLinkedInUrls.add(item.linkedIn);
+          }
+        }
+        const newStore = store.contacts
+          .filter(ct => ct.linkedIn == undefined || !updatingLinkedInUrls.has(ct.linkedIn))
+          .concat(msg.items);
+        await chrome.storage.sync.set({plinq: {...store, contacts: newStore}});
+        respond({status: 'ok'});
+      }
+      res();
+      return true;
     }
-    res();
-    return true;
   }
 });
+
+// Communications with LinkedIn sidebar
+// Step 1: who in this list is already imported? Page sends LinkedIn IDs, extension returns indices of those added.
+// Step 2: add some users/remove some users. Page sends [ {firstName, lastName, email, phone, linkedIn} ] extension returns list of failures
