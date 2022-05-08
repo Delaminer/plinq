77a358a8304827336a6f444245846c072b3b148c
Michael Peng <michael@mcmoo.org>
2022-05-08 18:06:46 -0400

    Complete new popup page, remove SPA config

--- components/popupButton.jsx
+++ components/popupButton.jsx
@@ -1,10 +1,15 @@
 import { CgMathPlus } from "react-icons/cg";
 import { FiExternalLink } from "react-icons/fi";
 
-export default function PopupButton({ text, plus }) {
+const buttonStyle = {
+  boxShadow: '0px 0px 8px rgba(82, 60, 245, 0.15)',
+  borderRadius: '8px'
+}
+
+export default function PopupButton({ text, plus, ...props }) {
   return (
-    <div className="my-8 rounded-lg shadow-xl p-4">
-      <div>{text}</div>
+    <div className="rounded-lg p-4 m-2 flex justify-between items-center cursor-pointer" style={buttonStyle} {...props}>
+      <b className="text-base">{text}</b>
       {plus ? <CgMathPlus /> : <FiExternalLink />}
     </div>
   );
--- pages/popup.js
+++ pages/popup.js
@@ -2,17 +2,28 @@ import React from "react";
 import PopupButton from "../components/popupButton";
 
 export default function Popup() {
+
+  async function open(target) {
+    await chrome.tabs.create({active: true, url: target});
+  }
+
+  async function addFromLinkedIn() {
+    await chrome.storage.local.set({'liImporting': true});
+    await open('https://www.linkedin.com/mynetwork/invite-connect/connections/');
+  }
+
   return (
     <React.Fragment>
-      <div className="pl-24 bg-white">
+      <div className="pl-4 bg-white">
         <img src="/logo.svg" className="pt-2.5"></img>
       </div>
-      <div className="flex flex-col">
-        <PopupButton text={"Add from LinkedIn"} plus={true}></PopupButton>
+      <div className="flex flex-col p-4" style={{width: '16rem'}}>
+        <PopupButton text={"Add from LinkedIn"} plus={true} onClick={addFromLinkedIn}></PopupButton>
         <PopupButton text={"Add manually"} plus={true}></PopupButton>
-        <PopupButton text={"Reminders"} plus={false}></PopupButton>
-        <PopupButton text={"Networks"} plus={false}></PopupButton>
-        <PopupButton text={"Templates"} plus={false}></PopupButton>
+        <hr className="my-4" />
+        <PopupButton text={"Reminders"} plus={false} onClick={() => open('index.html#follow-up')}></PopupButton>
+        <PopupButton text={"Networks"} plus={false} onClick={() => open('index.html#my-networks')}></PopupButton>
+        <PopupButton text={"Templates"} plus={false} onClick={() => open('index.html#templates')}></PopupButton>
       </div>
     </React.Fragment>
   );
