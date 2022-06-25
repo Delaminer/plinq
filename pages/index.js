336242b755d48e4033de283de862e0e01b9e9f67
Pasha Khomchenko <pavel.kh005@gmail.com>
2022-06-25 13:06:38 -0400

    Fix font size in extension

--- pages/index.js
+++ pages/index.js
@@ -232,6 +232,7 @@ export default function Home() {
     editTemplate(state.templates.length - 1, template);
   };
 
+<<<<<<< HEAD
   const editTemplate = (index, template) => {
     state.templates[index] = template;
     setState({ ...state });
@@ -244,6 +245,8 @@ export default function Home() {
     setState({ ...state });
   };
 
+=======
+>>>>>>> e5d353f (Fix font size in extension)
   return (
     <div>
       <div className="bg-white">
